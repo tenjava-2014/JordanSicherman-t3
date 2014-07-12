f999c53952d38a8085f9ccd721bce2c0750f8bb9
JordanSicherman <jordan.sicherman@brentwood.bc.ca>
2014-07-12 07:05:17 -0700

    I've never been good at using m2e (Maven to Eclipse)... Hopefully this is close enough :3

---
+++ src/main/java/com/tenjava/entries/JordanSicherman/t3/TenJava.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package main.java.com.tenjava.entries.JordanSicherman.t3;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Jordan
+ *
+ */
+public class TenJava extends JavaPlugin {
+
+}
